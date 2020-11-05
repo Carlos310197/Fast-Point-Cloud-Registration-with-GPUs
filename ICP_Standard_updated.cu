a3ab868810d7ee12677a15a7ee82948e1f578d93
Carlos310197 <66190532+Carlos310197@users.noreply.github.com>
2020-11-04 19:42:09 -0500

    Update ICP_Standard_updated.cu

--- ICP_Standard_updated.cu
+++ ICP_Standard_updated.cu
@@ -14,8 +14,8 @@
 //#include <device_functions.h>
 
 //constants
-#define WIDTH 10
-#define NUM_POINTS WIDTH*WIDTH //width of grid
+#define WIDTH 1<<5
+#define NUM_POINTS WIDTH*WIDTH //width of grid (1024 points)
 #define XY_min -2.0
 #define XY_max 2.0
 #define MAX_ITER 2
@@ -146,28 +146,27 @@ void Error(double* aux, double* D, double* M, int* idx, double* error, int itera
 
 int main()
 {
-	int num_points, i, j, k;
-	double ti[3], ri[3];
-	double lin_space[WIDTH], lenght;
-	double* mesh_x = NULL, * mesh_y = NULL, * z = NULL;
-	double* h_D = NULL, * h_M = NULL;
+	int i, j, k;
+	float ti[3], ri[3];
+	float lin_space[WIDTH], lenght;
+	float* mesh_x = NULL, * mesh_y = NULL, * z = NULL;
+	float* h_D = NULL, * h_M = NULL;
 
-	num_points = WIDTH * WIDTH;//number of points
 	lenght = XY_max - XY_min;
 
-	size_t bytesD = NUM_POINTS * 3 * sizeof(double);
-	size_t bytesM = NUM_POINTS * 3 * sizeof(double);
+	size_t bytesD = NUM_POINTS * 3 * sizeof(float);
+	size_t bytesM = NUM_POINTS * 3 * sizeof(float);
 
 	//create an array with all points equally separated
 	int n = WIDTH;
 	for (i = 0; i < WIDTH; i++)
 	{
-		lin_space[i] = XY_min + double(i) * (lenght) / (double(n) - 1.0f);
+		lin_space[i] = XY_min + float(i) * (lenght) / (float(n) - 1.0f);
 	}
 
 	//create meshgrid for x and y coordinates
-	mesh_x = (double*)malloc(num_points * sizeof(double));
-	mesh_y = (double*)malloc(num_points * sizeof(double));
+	mesh_x = (float*)malloc(num_points * sizeof(float));
+	mesh_y = (float*)malloc(num_points * sizeof(float));
 	i = 0;
 	k = 0;
 	while (i < num_points)
@@ -183,12 +182,12 @@ int main()
 	}
 
 	//Create the function z = f(x,y) = x^2-y^2
-	z = (double*)malloc(num_points * sizeof(double));
+	z = (float*)malloc(num_points * sizeof(float));
 	for (i = 0; i < num_points; i++) z[i] = pow(mesh_x[i], 2) - pow(mesh_y[i], 2);
 
 	//Create data point cloud matrix (source point cloud)
 	//points are stored in this order: x1y1z1, x2y2z2, x3y3z3, ....
-	h_D = (double*)malloc(bytesD);
+	h_D = (float*)malloc(bytesD);
 	k = 0;
 	for (i = 0; i < num_points; i++)
 	{
@@ -206,7 +205,7 @@ int main()
 
 	//Create model point cloud matrix (target point cloud)
 	//every matrix is defined using the colum-major order
-	h_M = (double*)malloc(bytesM);
+	h_M = (float*)malloc(bytesM);
 
 	//Translation values
 	ti[0] = 1.0f;//x
@@ -218,15 +217,15 @@ int main()
 	ri[1] = -0.5f;//axis y
 	ri[2] = 0.05f;//axis z
 
-	double h_rx[9] = {
+	float h_rx[9] = {
 		1.0f, 0.0f, 0.0f,
 		0.0f, cos(ri[0]), sin(ri[0]),
 		0.0f, -sin(ri[0]), cos(ri[0]) };
-	double h_ry[9] = {
+	float h_ry[9] = {
 		cos(ri[1]), 0.0f, -sin(ri[1]),
 		0.0f, 1, 0.0f,
 		sin(ri[1]), 0.0f, cos(ri[1]) };
-	double h_rz[9] = {
+	float h_rz[9] = {
 		cos(ri[2]), sin(ri[2]),0.0f,
 		-sin(ri[2]), cos(ri[2]), 0.0f,
 		0.0f, 0.0f, 1.0f };
@@ -235,18 +234,9 @@ int main()
 	/*double h_r[9] = {};
 	dmatrixMul(h_rx, h_ry, h_r, 3, 3, 3);
 	dmatrixMul(h_r, h_rz, h_r, 3, 3, 3);*/
-	double h_r[9] = {0.876485812,-0.37591464,0.300767018,
-					-0.04386084,0.559789799,0.827473024,
-					-0.47942553,-0.73846026,0.474159881};
-
-	/*printf("Rx:\n");
-	print_cloud(h_rx,3,3);
-	printf("Ry:\n");
-	print_cloud(h_ry,3,3);
-	printf("Rz:\n");
-	print_cloud(h_rz,3,3);
-	printf("Rotation Matrix:\n");
-	print_cloud(h_r,3,3);*/
+	float h_r[9] = {0.876485812,-0.37591464,0.300767018,
+			-0.04386084,0.559789799,0.827473024,
+			-0.47942553,-0.73846026,0.474159881};
 
 	//h_M = h_r*h_D
 	dmatrixMul(h_r, h_D, h_M, 3, NUM_POINTS, 3);
@@ -270,31 +260,31 @@ int main()
 	int D_size = NUM_POINTS;//data cloud number of points
 	int M_size = NUM_POINTS;//model cloud number of points
 
-	double* d_M = NULL;//model cloud (device)
-	double* d_Dt = NULL;//data cloud transformed (device)
+	float* d_M = NULL;//model cloud (device)
+	float* d_Dt = NULL;//data cloud transformed (device)
 	
 	int* d_idx = NULL;//index vector (used for correspondence)
 	int* h_idx = NULL;//index vector (used for correspondence)
 	
-	double* d_barD = NULL, * d_barM = NULL;//centroids (device)
-	double* d_devD = NULL, * d_devM = NULL;//deviations (device)
+	float* d_barD = NULL, * d_barM = NULL;//centroids (device)
+	float* d_devD = NULL, * d_devM = NULL;//deviations (device)
 	
-	double* d_W = NULL;//3x3 matrix used in SVD
-	double* d_S = NULL;
-	double* d_U = NULL;
-	double* d_VT = NULL;
-	double* d_work = NULL;
-	double* d_rwork = NULL;
+	float* d_W = NULL;//3x3 matrix used in SVD
+	float* d_S = NULL;
+	float* d_U = NULL;
+	float* d_VT = NULL;
+	float* d_work = NULL;
+	float* d_rwork = NULL;
 	int* devInfo = NULL;
 	int lwork = 0;
-	double alpha, beta;
+	float alpha, beta;
 
-	double* d_temp_r = NULL;
-	double* d_temp_T = NULL;
-	double* rep_T = NULL;
+	float* d_temp_r = NULL;
+	float* d_temp_T = NULL;
+	float* rep_T = NULL;
 
-	double* d_error = NULL;
-	double* h_error = NULL;
+	float* d_error = NULL;
+	float* h_error = NULL;
 
 	h_idx = (int*)malloc(D_size * sizeof(int));
 	cudaMalloc(&d_idx, D_size * sizeof(int));
@@ -313,47 +303,37 @@ int main()
 	cudaMemset(d_barD, 0, 3 * sizeof(double));
 	cudaMemset(d_barM, 0, 3 * sizeof(double));
 
-	//n gets the nearest greater power of 2
-	n = 0;
-    int q = round(log2(NUM_POINTS));
-    if(q % 2 == 0)
-    {
-        n = 1 << q;
-        if(n < NUM_POINTS) n = 1 << (q+2);
-    }
-	else n = 1 << (q+1);
-
-	cudaMalloc(&d_devD, 3*n*sizeof(double));
-	cudaMalloc(&d_devM, 3*n*sizeof(double));
-	cudaMemset(d_devD, 0, 3*n*sizeof(double));
-	cudaMemset(d_devM, 0, 3*n*sizeof(double));
+	cudaMalloc(&d_devD, bytesD);
+	cudaMalloc(&d_devM, bytesD);
+	cudaMemset(d_devD, 0, bytesD);
+	cudaMemset(d_devM, 0, bytesD);
 
 	//double *h_Dt = (double*)malloc(3*n*sizeof(double));
 	
-	double* h_barD, * h_barM;
-	double* h_devD, * h_devM;
-	h_barD = (double*)malloc(3 * sizeof(double));
-	h_barM = (double*)malloc(3 * sizeof(double));
-	h_devD = (double*)malloc(bytesD);
-	h_devM = (double*)malloc(bytesD);
+	float* h_barD, * h_barM;
+	float* h_devD, * h_devM;
+	h_barD = (float*)malloc(3 * sizeof(float));
+	h_barM = (float*)malloc(3 * sizeof(float));
+	h_devD = (float*)malloc(bytesD);
+	h_devM = (float*)malloc(bytesD);
 
-	cudaMalloc(&d_S, sizeof(double) * 3);
-	cudaMalloc(&d_U, sizeof(double) * 9);
-	cudaMalloc(&d_VT, sizeof(double) * 9);
+	cudaMalloc(&d_S, sizeof(float) * 3);
+	cudaMalloc(&d_U, sizeof(float) * 9);
+	cudaMalloc(&d_VT, sizeof(float) * 9);
 	cudaMalloc(&devInfo, sizeof(int));
-	cudaMalloc(&d_W, sizeof(double) * 9);
+	cudaMalloc(&d_W, sizeof(float) * 9);
 	cusolverDnHandle_t cusolverH;//cuSolver handle
 	cusolverDnCreate(&cusolverH);
 	cusolverDnDgesvd_bufferSize(cusolverH, 3, 3, &lwork);
-	cudaMalloc((void**)&d_work, sizeof(double) * lwork);
+	cudaMalloc((void**)&d_work, sizeof(float) * lwork);
 
-	cudaMalloc(&d_temp_r, sizeof(double) * 9);
-	cudaMalloc(&d_temp_T, sizeof(double) * 3);
-	cudaMalloc(&rep_T, sizeof(double) * 3 * D_size);
+	cudaMalloc(&d_temp_r, sizeof(float) * 9);
+	cudaMalloc(&d_temp_T, sizeof(float) * 3);
+	cudaMalloc(&rep_T, sizeof(float) * 3 * D_size);
 
-	cudaMalloc(&d_error, MAX_ITER * sizeof(double));
-	cudaMemset(d_error, 0, MAX_ITER * sizeof(double));
-	h_error = (double*)malloc(MAX_ITER * sizeof(double));
+	cudaMalloc(&d_error, MAX_ITER * sizeof(float));
+	cudaMemset(d_error, 0, MAX_ITER * sizeof(float));
+	h_error = (float*)malloc(MAX_ITER * sizeof(float));
 
 	cudaError_t err;//for checking errors in kernels
 
